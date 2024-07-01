27e03a0d54c3f80428e3ff8ced7c6698bc176267
gusty <gustavodiorlando@gmail.com>
2024-07-01 19:47:08 -0300

    Add form for posting messages

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Message Board', messages: messages});
 });
 
+router.post('/', function(req, res, next) {
+  console.log(req.body)
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.messageUser,
+    added: new Date()
+  })
+  res.redirect('/')
+})
+
 module.exports = router;
