5f835aaab30a1c3da20f9b3247bcc3a9d22a7a05
gusty <gustavodiorlando@gmail.com>
2024-07-02 12:56:35 -0300

    Remove unused users route and replace var with const

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,6 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  console.log(req.body)
   messages.unshift({
     text: req.body.messageText,
     user: req.body.messageUser,
