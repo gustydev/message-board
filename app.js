8b7cfc8e1dbf47fbc2cf740b4eb16cc6780499a9
gusty <gustavodiorlando@gmail.com>
2024-07-05 15:28:55 -0300

    Install modules and settings for production

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require("compression");
+const helmet = require("helmet");
 
 const indexRouter = require('./routes/index');
 
@@ -22,11 +24,22 @@ async function main() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(compression()); // Compress all routes
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up rate limiter
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 50,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 
 app.use('/', indexRouter);
 
